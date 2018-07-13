d7daa9598f0f6e2b1a5d6fefe195c320c0b7f05a
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-07-13 11:13:26 +0530

    Update cx_freeze-setup.py

--- cx_freeze-setup.py
+++ cx_freeze-setup.py
@@ -2,8 +2,6 @@ from cx_Freeze import setup, Executable
 import os
 
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-#os.environ['TCL_LIBRARY'] = "C:\\Users\\Agranya Pratap Singh\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
-#os.environ['TK_LIBRARY'] = "C:\\Users\\Agranya Pratap Singh\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
 print(PYTHON_INSTALL_DIR)
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
@@ -20,4 +18,4 @@ setup(options = options,
       name = "AutoMail" ,
       version = "1.0" ,
       description = "" ,
-      executables = [Executable("final.py")])
+      executables = [Executable("automate.py")])
